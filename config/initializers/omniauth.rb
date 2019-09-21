f15a53681c2cfaebecbd50fcfa5520a1e0e7faa2
trevoruptain <trevoruptain@gmail.com>
2019-09-20 22:45:16 -0500

    Fix dumb typo in omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 require '../../keys/auth_keys.rb'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, , auth_keys.key || ENV['GOOGLE_CLIENT_ID'], auth_keys.secret || ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, auth_keys.key || ENV['GOOGLE_CLIENT_ID'], auth_keys.secret || ENV['GOOGLE_CLIENT_SECRET']
 end
 
 # Rails.application.config.middleware.use OmniAuth::Builder do
