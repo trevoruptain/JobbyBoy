a0ff63cf37119d9ccecbbce9a2e8e228ed2d244a
trevoruptain <trevoruptain@gmail.com>
2019-09-20 23:52:30 -0500

    Add React

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], redirect_uri: 'http://jobbyboy.herokuapp.com'
 end
-
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
-# end
