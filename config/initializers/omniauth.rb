d23de80e048cef4c7bf9a0f8ad5b3a8149fc9892
trevoruptain <trevoruptain@gmail.com>
2019-09-20 23:14:09 -0500

    Update redirect URI for oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], redirect_uri: '/'
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], redirect_uri: 'http%3A%2F%2Fjobbyboy.herokuapp.com'
 end
 
 # Rails.application.config.middleware.use OmniAuth::Builder do
